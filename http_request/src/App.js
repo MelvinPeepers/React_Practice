02d2bc3f398397fc089d0a91b0a90d3863be870b
Melvin <peepersm@gmail.com>
2020-08-22 19:56:11 -0400

    completed review of HTML Request

--- http_request/src/App.js
+++ http_request/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: ''
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    console.log('Success!')
+    axios.get('https://api.github.com/users/depadiernos')
+      .then(response => this.setState({username: response.data.name}))
+  }
+
+  render() {
+    return (
+      <div className="button__container">
+        <button className="button" onClick={this.handleClick}>Click Me</button>
+    <p>{this.state.username}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
